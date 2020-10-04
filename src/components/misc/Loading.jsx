63ae8babd7d48fd41b9b0aa94d8286099ca06acf
Luke Gibbons <32064971+lukegib@users.noreply.github.com>
2020-10-04 15:06:52 +0100

    Removed idea to change loading icon

--- src/components/misc/Loading.jsx
+++ src/components/misc/Loading.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import styles from './Loading.module.css';
 import loadingSpinner from '../../assets/loading.png';
 
-// TODO: Change the spinner into a sun
-
 const Loading = () => {
     return (
         <div className={styles.loading}>
